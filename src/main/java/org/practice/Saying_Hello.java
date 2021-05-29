10ea7c94067c0c5666693b01187deaa6c3f41e43
Sebastian <SebastianLSteele@knights.ucf.edu>
2021-05-29 15:55:49 -0400

    Created Class, and finished ex.1

--- src/main/java/org/practice/Saying_Hello.java
+++ src/main/java/org/practice/Saying_Hello.java
@@ -1,5 +1,17 @@
 package org.practice;
+import java.util.Scanner;
 
 public class Saying_Hello {
+    public static void main(String[] args) {
+        // lets get a name
+        Scanner getName = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String Name = getName.nextLine();
 
+        // putting the output string together
+        String conCat = "Hello, "+ Name+", nice to meet you!";
+
+        // outputting separately as requested
+        System.out.println(conCat);
+    }
 }
